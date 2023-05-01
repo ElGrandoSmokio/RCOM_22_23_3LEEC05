5a6426b20ecbf1da660785f6eafd5f6231ecae33
ElGrandoSmokio <78943412+ElGrandoSmokio@users.noreply.github.com>
2023-05-01 03:43:17 +0100

    Add files via upload
    Implementação da máquina de estados para frames UA e SET

--- receiver.c
+++ receiver.c
@@ -11,16 +11,100 @@
 #define FALSE 0
 #define TRUE 1
 
+#define FLAG 0x5C
+#define A_tx 0x01  // Tx -> Rx -> Tx
+#define A_rx 0x03  // Rx -> Tx -> Rx
+#define C_SET 0x03
+#define C_DISC 0x11
+#define C_UA 0x07
+#define BCC_SET A_tx ^ C_SET
+
+unsigned char frameUA[5];
+
+typedef enum{
+    START,
+    FLAG_RCV,
+    A_RCV,
+    C_RCV,
+    BCC_OK,
+    STOP
+} SETstateMachineStates;
+
+SETstateMachineStates currentStateSET = START;
+
+void stateMachineSET(unsigned char block){
+    unsigned char A, C;
+
+    switch (currentStateSET)
+    {
+    case START:
+        if(block == FLAG){
+            currentStateSET = FLAG_RCV;
+            break;
+        }else{
+            break;
+        }
+    case FLAG_RCV:
+        if(block == A_tx){
+            A = block;
+            currentStateSET = A_RCV;
+            break;
+        }else if(block == FLAG){
+            break;
+        }else{
+            currentStateSET = START;
+            break;
+        }
+    case A_RCV:
+        if(block == C_SET){
+            C = block;
+            currentStateSET = C_RCV;
+            break;
+        }else if(block == FLAG){
+            currentStateSET = FLAG_RCV;
+            break;
+        }else{
+            currentStateSET = START;
+            break;
+        }
+    case C_RCV:
+        if((A ^ C) == BCC_SET){
+            currentStateSET = BCC_OK;
+            break;
+        }else if(block == FLAG){
+            currentStateSET = FLAG_RCV;
+            break;
+        }else{
+            currentStateSET = START;
+            break;
+        }
+    case BCC_OK:
+        printf("SET RECEBIDO\n");
+        if(block == FLAG){
+            currentStateSET = STOP;
+            break;
+        }else{
+            currentStateSET = START;
+            break;
+        }
+    case STOP:
+        break;
+    }
+    
+}
+
 int main(int argc, char** argv) {
 
-    char buf[255];
-    ssize_t bytesRead;
+    unsigned char buf[1];
+    ssize_t bytesRead, bytesWritten;
     struct termios oldtio,newtio;
 
-    if (argc < 2) {
+    /* if (argc < 2) {
         printf("Usage: %s <serial_port>\n", argv[0]);
         exit(1);
-    }
+    } */
+
+    argv[1] = "/dev/ttyS10";
 
     int fd = open(argv[1], O_RDWR | O_NOCTTY );
     if (fd < 0) { perror(argv[1]); exit(-1); }
@@ -39,7 +123,7 @@ int main(int argc, char** argv) {
     newtio.c_lflag = 0;
 
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
 
 
 
@@ -58,27 +142,46 @@ int main(int argc, char** argv) {
 
     printf("New termios structure set\n");
 
-    printf("Reading from serial port...\n");
+    
     while (1) {
-
-        bzero(buf, sizeof(buf));
-        bytesRead = read(fd, buf, sizeof(buf));
+        printf("Reading from serial port...\n");
+        //bzero(buf, sizeof(buf));
+        bytesRead = read(fd, buf, 1);
         if (bytesRead > 0) {
-            printf("Received: %s\n", buf);
+            printf("Received: %x\n", buf[0]);
         } else if (bytesRead == 0) {
             printf("No data received\n");
         } else {
             perror("Failed to read from serial port");
             return 1;
         }
+
+        if(buf[0] == FLAG){
+            printf("FLAG RECEBIDA\n");
+            printf("%x\n", buf[0]);
+        }
+        stateMachineSET(buf[0]);
+
+        if(currentStateSET == STOP){
+            frameUA[0] = FLAG;
+            frameUA[1] = A_tx;
+            frameUA[2] = C_UA;
+            frameUA[3] = frameUA[1] ^ frameUA[2];
+            frameUA[4] = FLAG;
+
+            bytesWritten = write(fd, frameUA, 5);
+        }
         
-        printf("BUFFER: %s\n", buf);
-        write(fd, buf, strlen(buf));
-        printf("Returned: %s\n", buf);
-        sleep(0.3);
+        //printf("BUFFER: %x\n", buf);
+        /* write(fd, buf, strlen(buf));
+        printf("Returned: %s\n", buf); */
+        //sleep(0.3);
     }
     
 
     close(fd);
     return 0;
 }
+
+
+
--- transmitter.c
+++ transmitter.c
@@ -5,12 +5,101 @@
 #include <unistd.h>
 #include <termios.h>
 
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+
+#define FLAG 0x5C
+#define A_tx 0x01  // Tx -> Rx -> Tx
+#define A_rx 0x03  // Rx -> Tx -> Rx
+#define C_SET 0x03
+#define C_DISC 0x11
+#define C_UA 0x07
+#define BCC_SET A_tx ^ C_SET
+#define BCC_UA A_tx ^ C_UA
+
+typedef enum{
+    START,
+    FLAG_RCV,
+    A_RCV,
+    C_RCV,
+    BCC_OK,
+    STOP
+} UAstateMachineStates;
+
+UAstateMachineStates currentStateUA = START;
+
+void stateMachineUA(unsigned char block){
+    unsigned char A, C;
+
+    switch (currentStateUA)
+    {
+    case START:
+        if(block == FLAG){
+            currentStateUA = FLAG_RCV;
+            break;
+        }else{
+            break;
+        }
+    case FLAG_RCV:
+        if(block == A_tx){
+            A = block;
+            currentStateUA = A_RCV;
+            break;
+        }else if(block == FLAG){
+            break;
+        }else{
+            currentStateUA = START;
+            break;
+        }
+    case A_RCV:
+        if(block == C_UA){
+            C = block;
+            currentStateUA = C_RCV;
+            break;
+        }else if(block == FLAG){
+            currentStateUA = FLAG_RCV;
+            break;
+        }else{
+            currentStateUA = START;
+            break;
+        }
+    case C_RCV:
+        if((A ^ C) == BCC_UA){
+            currentStateUA = BCC_OK;
+            break;
+        }else if(block == FLAG){
+            currentStateUA = FLAG_RCV;
+            break;
+        }else{
+            currentStateUA = START;
+            break;
+        }
+    case BCC_OK:
+        printf("UA RECEBIDO\n");
+        if(block == FLAG){
+            currentStateUA = STOP;
+            break;
+        }else{
+            currentStateUA = START;
+            break;
+        }
+    case STOP:
+        break;
+    }
+    
+}
+
+
 int main(int argc, char** argv)
 {
-    if (argc != 2) {
+    /* if (argc != 2) {
         printf("Usage: %s <serial_port>\n", argv[0]);
         exit(1);
-    }
+    } */
+
+    argv[1] = "/dev/ttyS11";
 
     int fd = open(argv[1], O_RDWR | O_NOCTTY);
     if (fd == -1) {
@@ -38,20 +127,38 @@ int main(int argc, char** argv)
         exit(1);
     }
 
-    char buffer[255];
-    ssize_t num_bytes;
+    unsigned char frame[5];
+    char buffer[1];
+    ssize_t num_bytes, bytes_read;
+    
+    frame[0] = FLAG;
+    frame[1] = A_tx;
+    frame[2] = C_SET;
+    frame[3] = BCC_SET;
+    frame[4] = FLAG;
+
 
     while (1) {
 
         gets(buffer);
-        printf("%s\n", buffer);
-        num_bytes = write(fd, buffer, strlen(buffer));
+        /* printf("%s\n", buffer); */
+        printf("SENDING FRAME\n");
+        num_bytes = write(fd, frame, sizeof(frame));
         if (num_bytes == -1) {
             perror("Error writing to serial port");
             close(fd);
             exit(1);
         }
         printf("Wrote %ld bytes to serial port.\n", num_bytes);
+        while (1){
+            bytes_read = read(fd, buffer, 1);
+            printf("Received: %x\n", buffer[0]);
+            if(buffer[0] == FLAG){
+                printf("FLAG RECEBIDA\n");
+                printf("%x\n", buffer[0]);
+            }
+            stateMachineUA(buffer[0]);
+        }
     }
 
     close(fd);
